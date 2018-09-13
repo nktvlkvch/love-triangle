7bcf2568c6b4d0966cc0ff5619be0be6a0a26dd2
Nikita Valkevich <n.valkevich@icloud.com>
2018-09-13 23:44:05 +0300

    Task 2

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var arrayLenght = preferences.length;
+  var count = 0;
+  for(var i = 0; i<arrayLenght; i++){
+  	var next = preferences[i] - 1;
+  	if(next == i) continue;
+  	var next2 = preferences[next] - 1;
+  	if(next == next2) continue;
+  	var next3 = preferences[next2] - 1;
+  	if(next3 == next2) continue;
+  	if(next3 == i){
+  		count++;
+  	}
+  }
+  return count / 3;
 };
